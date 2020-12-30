9b45e4b7f7ce6c2ed830eb9b65052eea94250a76
Jskallio <32382026+Jskallio@users.noreply.github.com>
2020-12-30 19:54:45 +0200

    Update config.py

--- software/config.py
+++ software/config.py
@@ -7,4 +7,4 @@ SUDO_PASS = "SoftwareProject"
 #Command that will run on students command line when they connect with ssh
 BASHRC_FILE_COMMAND = "python /home/developer/Software/start.py"
 
-MOTD_PATH = "home/developer/Software/motd.txt"
+MOTD_PATH = "/home/developer/Software/motd.txt"
