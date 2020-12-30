0b0ad62017750b2538bfc788b543a2c2f6f01250
Juho Kalliokoski <jskalliokoski@gmail.com>
2020-12-30 18:55:34 +0200

    edited start.py

--- config.py
+++ config.py
@@ -1,8 +1,3 @@
 #Path to the database file
 SQLITE_DATABASE_PATH = "sqlite:////home/developer/database/chipwhisperer.db"
-#Path to the chipwhisperer folder that will be copied to users
-CHIPWHISPERER_FOLDER = "/home/developer/Software/chipwhisperer"
-#Password for sudo
-SUDO_PASS = "SoftwareProject"
-#Command that will run on students command line when they connect with ssh
-BASHRC_FILE_COMMAND = "python /home/developer/Software/start.py"
+
