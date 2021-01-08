b40d575321029262a45244d40885e337170f61d2
Juho Kalliokoski <jskalliokoski@gmail.com>
2021-01-08 12:37:39 +0200

    Added option to empty vm in manage_vms.py

--- software/manage_vms.py
+++ software/manage_vms.py
@@ -1,6 +1,10 @@
 import database_commands
+import vm_commands
 import sys
 if sys.argv[1] == "A":
     print(database_commands.add_vm(sys.argv[2]))
 elif sys.argv[1] == "R":
     print(database_commands.delete_vm(sys.argv[2]))
+elif sys.argv[1] == "E":
+    print(database_commands.empty_vm(sys.argv[2]))
+    vm_commands.CloseVM(sys.argv[2])
